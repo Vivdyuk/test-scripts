884cc18fdb1b700c6c52c3a4d86f6241c6c04eb7
Ruslan Vyvdyuk <vivdyuk.ruslan@gmail.com>
2021-08-26 10:38:46 +0300

    C6:I15 was refactored

--- operating-budget-C6-I15.js
+++ operating-budget-C6-I15.js
@@ -4,8 +4,8 @@ function myFunction(doc, range) {
     const activeSheet = spreadSheet.getSheetByName('Operating Budget');
     const activeRange = activeSheet.getActiveRange();
 
-    if (activeRange.getWidth() != 7 || activeRange.getA1Notation() !== 'C6:I15') {
-        throw new Error(`'choose a correct range in a sheet, not ${activeRange.getA1Notation()}'`)
+    if (activeRange.getWidth() != 7) {
+        throw new Error('range has to be 7 columns wide')
     }
 
     const sqlSheet = spreadSheet.getSheetByName('SQL Staff');
@@ -16,44 +16,41 @@ function myFunction(doc, range) {
 
     const firstRow = activeRange.getRow();
     const lastRow = activeRange.getLastRow();
-    const formulas = [];
-    let iteratee = 0;
-
-    while(formulas.length < 8) {
-        formulas.push(
-            [`=SUMIF('SQL Staff'!$G:$G,$A${firstRow + iteratee},'SQL Staff'!A:A)`,
-                ``,
-                `=SUMIF('SQL Staff'!$G:$G,$A${firstRow + iteratee},'SQL Staff'!B:B)`,
-                ``,
-                `=SUMIF('SQL Staff'!$G:$G,$A${firstRow + iteratee},'SQL Staff'!C:C)`,
-                ``,
-                `=SUMIF('SQL Staff'!$G:$G,B${firstRow + iteratee},'SQL Staff'!D:D)`
+    const formulas = _.times((lastRow - firstRow + 1), n => {
+        if (activeSheet.getRange(`B${firstRow + n}`).getValue().startsWith('Total')) {
+            return [
+                `=SUM(C${firstRow}:C${lastRow - 2})`,
+                '',
+                `=SUM(E${firstRow}:E${lastRow - 2})`,
+                '',
+                `=SUM(G${firstRow}:G${lastRow - 2})`,
+                '',
+                `=SUM(I${firstRow}:I${lastRow - 2})`
             ]
-        );
+        }
 
-        iteratee++;
-    }
-
-    formulas.push(
-        [
-            `=SUM(C${firstRow - 1}:C${lastRow - 2})`,
-            '',
-            `=SUM(E${firstRow - 1}:E${lastRow - 2})`,
-            '',
-            `=SUM(G${firstRow - 1}:G${lastRow - 2})`,
-            '',
-            `=SUM(I${firstRow - 1}:I${lastRow - 2})`
-        ],
-        [
-            `=C${lastRow - 1}`,
-            '',
-            `=E${lastRow - 1}`,
-            '',
-            `=G${lastRow - 1}`,
-            '',
-            `=I${lastRow - 1}`,
-        ]
-    )
+        if (activeSheet.getRange(`B${firstRow +n}`).getValue().startsWith('TOTAL')) {
+            return [
+                `=C${lastRow - 1}`,
+                '',
+                `=E${lastRow - 1}`,
+                '',
+                `=G${lastRow - 1}`,
+                '',
+                `=I${lastRow - 1}`
+            ]
+        }
+        return (
+            [`=SUMIF('SQL Staff'!$G:$G,$A${firstRow + n},'SQL Staff'!A:A)`,
+                ``,
+                `=SUMIF('SQL Staff'!$G:$G,$A${firstRow + n},'SQL Staff'!B:B)`,
+                ``,
+                `=SUMIF('SQL Staff'!$G:$G,$A${firstRow + n},'SQL Staff'!C:C)`,
+                ``,
+                `=SUMIF('SQL Staff'!$G:$G,B${firstRow + n},'SQL Staff'!D:D)`
+            ]
+        )
+    })
 
     activeRange.setFormulas(formulas);
 }
